d2994d7e914a7ac035c777994ef841cf77f57d39
StevenCarrilloLoor <stevencarrilloloor@gmail.com>
2024-07-04 07:18:07 -0500

    Agregar archivos de proyecto.

---
+++ Progreso3Paises/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progreso3Paises.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public string Status { get; set; }
+        public string Code { get; set; }
+    }
+}
